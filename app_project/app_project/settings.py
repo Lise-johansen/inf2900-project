7b1eb8d2f07420bb3ae17cfee42e08221e802ffa
Dybedahl <adriandybdal@outlook.com>
2024-01-31 13:24:44 +0100

    Add User model and migration files

--- app_project/app_project/settings.py
+++ app_project/app_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'airfinn.apps.AirfinnConfig',
+    'airfinn',
     
     # Django SSL extension
     'django_extensions',
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'app_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
