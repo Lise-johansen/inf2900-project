ba631d4d0bae3ce798cebc982f649b0b5c2588ed
Dybedahl <adriandybdal@outlook.com>
2024-02-07 13:10:52 +0100

    Add password reset functionality

--- app_project/airfinn/views.py
+++ app_project/airfinn/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.http import JsonResponse, HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
 import json
 
 def index(request):
@@ -52,3 +57,23 @@ def register(request):
     # Optionally, you can perform additional actions like sending a confirmation email
         
     return JsonResponse({'message': 'User registered successfully'}, status=201)
+
+def send_password_reset_email(request):
+    if request.method != 'POST':
+        return JsonResponse({'error': 'Method Not Allowed'}, status=405)
+        
+    data = json.loads(request.body)
+    
+    email = data.get('email')
+    user = User.objects.filter(email=email).first()
+    
+    if user:
+        token_generator = PasswordResetTokenGenerator()
+        uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+        token = token_generator.make_token(user)
+        reset_link = f"http://localhost:8080/reset-password/{uidb64}/{token}/"
+        email_body = render_to_string('password_reset_email.html', {'reset_link': reset_link})
+        send_mail('Password Reset', email_body, 'noreply@dybedahlserver.net', [email])
+        return JsonResponse({'message': 'Password reset email sent'}, status=200)
+    else:
+        return JsonResponse({'error': 'User not found'}, status=404)
