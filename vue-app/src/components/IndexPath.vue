2b90d3790aa0391d6f4d5e284aa6821aa567e36e
Heine Lysfjord <hly023@uit.no>
2024-02-07 14:17:32 +0100

    Add new assets and update styles in IndexPath.vue

--- vue-app/src/components/IndexPath.vue
+++ vue-app/src/components/IndexPath.vue
@@ -5,6 +5,7 @@
 
 <template>
 
+
         <div>
                 <h1>Welcome to the Index Page</h1>
         <button @click="redirectToLogin">Go to Login</button>
@@ -28,6 +29,25 @@ export default {
 </script>
 
 <style scoped>
+
+        h3 {
+        margin: 40px 0 0;
+        }
+        
+        ul {
+        list-style-type: none;
+        padding: 0;
+        }
+        
+        li {
+        display: inline-block;
+        margin: 0 10px;
+        }
+        
+        a {
+        color: #42b983;
+        }
+
 
         h3 {
         margin: 40px 0 0;
