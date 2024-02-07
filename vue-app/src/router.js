c345e6d1378a357913ecb4086a16520c6655c9dc
Dybedahl <adriandybdal@outlook.com>
2024-02-07 13:11:29 +0100

    Add PasswordReset component to router.js

--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from './components/LoginPath.vue'; // Import your Login component
 import Register from './components/RegisterPath.vue'; // Import your Register component
+import PasswordReset from './components/PasswordReset.vue'; // Import Password Reset component
 
 const routes = [
   {
@@ -14,6 +15,11 @@ const routes = [
     name: 'register',
     component: Register,
   },
+  {
+    path: '/reset',
+    name: 'reset',
+    component: PasswordReset
+  }
   // Other routes
 ];
 
