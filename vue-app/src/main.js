265a9ccf8aeb5d7fe59eaa78fd0f0db1ad451327
Johannes Figenschou <johannes@tromso-studenter6-1136.wifi.uit.no>
2024-02-15 14:08:56 +0100

    rm single empty line

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -4,11 +4,5 @@ import router from './router';
 
 const app = createApp(App);
 
-<<<<<<< HEAD
-
 // Allow the user to log in as usual
 app.use(router).mount('#app');
-=======
-// Allow the user to log in as usual
-app.use(router).mount('#app');
->>>>>>> localtest_adrian
