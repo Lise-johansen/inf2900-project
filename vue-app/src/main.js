5ef3a1e537664126c2aeb91e4b977e343654c205
Johannes <jfi025@uit.no>
2024-02-15 18:37:57 +0100

    Update SSL certificate and key

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import VueRouter from 'vue-router';
 import router from './router';
 
 const app = createApp(App);
 
-app.use(VueRouter);
-
-  // Allow the user to log in as usual
-  app.use(router).mount('#app');
+// Allow the user to log in as usual
+app.use(router).mount('#app');
