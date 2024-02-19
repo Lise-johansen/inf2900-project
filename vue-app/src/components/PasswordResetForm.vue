8db73ee69e11581c2f97271a8ef7c02dd48ba1cb
Dybedahl <adriandybdal@outlook.com>
2024-02-19 15:11:05 +0100

    Refactor PasswordResetForm component

--- vue-app/src/components/PasswordResetForm.vue
+++ vue-app/src/components/PasswordResetForm.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-      <input type="password" v-model="password" placeholder="New Password">
-      <input type="password" v-model="confirmPassword" placeholder="Confirm New Password">
-      <button @click="resetPassword">Reset Password</button>
-    </div>
-  </template>
+  <div>
+    <input type="password" v-model="password" placeholder="New Password">
+    <input type="password" v-model="confirmPassword" placeholder="Confirm New Password">
+    <button @click="resetPassword">Reset Password</button>
+  </div>
+</template>
   
-  <script>
+<script>
   import axiosInstance from '@/axios';
-  
+
   export default {
     data() {
       return {
@@ -17,32 +17,32 @@
       };
     },
     methods: {
-        resetPassword() {
-            if (this.password !== this.confirmPassword) {
-                alert('Passwords do not match');
-                return;
-            }
-
-            const uidb64 = this.$route.params.uidb64;
-            const token = this.$route.params.token;
-
-            axiosInstance.post(`reset-password/${uidb64}/${token}/`, {
-                new_password1: this.password, // Ensure both new_password1 and new_password2 are provided
-                new_password2: this.confirmPassword
-            })
-            .then(() => {
-                alert('Password reset successfully');
-                // Optionally, redirect the user to another page
-            })
-            .catch((error) => {
-                if (error && error.response && error.response.data && error.response.data.error) {
-                    alert('Error resetting password: ' + error.response.data.error); // Display error message from server
-                } else {
-                    alert('Error resetting password: Unknown error'); // Display generic error message
-                }
-            });
+      resetPassword() {
+        if (this.password !== this.confirmPassword) {
+          alert('Passwords do not match');
+          return;
         }
+
+        const uidb64 = this.$route.params.uidb64;
+        const token = this.$route.params.token;
+
+        axiosInstance.post(`reset-password/${uidb64}/${token}/`, {
+          new_password1: this.password, // Ensure both new_password1 and new_password2 are provided
+          new_password2: this.confirmPassword
+        })
+        .then(() => {
+          alert('Password reset successfully');
+          // Optionally, redirect the user to another page
+        })
+        .catch((error) => {
+          if (error && error.response && error.response.data && error.response.data.error) {
+            alert('Error resetting password: ' + error.response.data.error); // Display error message from server
+          } else {
+            alert('Error resetting password: Unknown error'); // Display generic error message
+          }
+        });
       }
     }
-  </script>
+  }
+</script>
   
