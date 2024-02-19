0cc297d66b8ad5704e9a45314f17922a1fa952c3
Dybedahl <adriandybdal@outlook.com>
2024-02-19 13:30:46 +0100

    Add reset password API endpoint

--- app_project/app_project/urls.py
+++ app_project/app_project/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('api/login/', views.login, name='login'),
     path('api/register/', views.register, name='register'),
     path('api/send-password-reset-email/', views.send_password_reset_email, name='send_password_reset_email'),
+    path('api/reset-password/<str:uidb64>/<str:token>/', views.reset_password, name='reset_password'),
     path('api/dashboard/', views.dashboard, name='dashboard'),
     path('api/search/', views.search_items, name='search_items'),
     path('api/logout/', views.logout, name='logout'),
