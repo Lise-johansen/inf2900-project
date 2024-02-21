35d65e485312d4b9ee6e175ff43a527a2daa2777
Solon <55138422+SolonNOR@users.noreply.github.com>
2024-02-21 21:15:13 +0100

    new feature in development

--- app_project/airfinn/models.py
+++ app_project/airfinn/models.py
@@ -11,3 +11,12 @@ class User(models.Model):
     
 class Item(models.Model):
     name = models.CharField(max_length = 100)
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.title
