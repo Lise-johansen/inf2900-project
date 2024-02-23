35fa3bdf20158178e2bad115e396e5c149ee6588
Dybedahl <adriandybdal@outlook.com>
2024-02-23 14:19:09 +0100

    Update axios base URL to localhost

--- vue-app/src/axios.js
+++ vue-app/src/axios.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://django.dybedahlserver.net/api/', // Use the domain Nginx forwards requests to
+  baseURL: 'http://localhost:8000/api/', // Use the domain Nginx forwards requests to
   withCredentials: false, // Ensure cookies are sent with cross-origin requests
 });
 
