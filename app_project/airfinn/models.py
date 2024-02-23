73a05b5f9f105bd0e084ec075df2f17aae522f58
Dybedahl <adriandybdal@outlook.com>
2024-02-23 11:56:34 +0100

    Add is_verified field to User model

--- app_project/airfinn/models.py
+++ app_project/airfinn/models.py
@@ -36,6 +36,7 @@ class User(AbstractBaseUser):
     created_at = models.DateTimeField(auto_now_add=True)
 
     is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
