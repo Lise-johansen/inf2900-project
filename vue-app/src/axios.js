c719025282bb81835578811de79495a023d805ce
Paal <pbr027@uit.no>
2024-03-08 11:51:22 +0100

    Short summary of the changes
    Description:
    Provide a brief description of the changes made in this commit.
    Updating branch with main.
    
    Type of change:
    - [ ] Bug fix (non-breaking change)
    - [ ] New feature (non-breaking change)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    
    Testing:
    Describe how these changes have been tested.
    
    Additional Notes:
    Add any additional notes or considerations here.

--- vue-app/src/axios.js
+++ vue-app/src/axios.js
@@ -3,8 +3,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://django.dybedahlserver.net/api/', // Use the domain Nginx forwards requests to
-  withCredentials: true, // Ensure cookies are sent with cross-origin requests
+  baseURL: 'http://localhost:8000', // Use the domain Nginx forwards requests to
+  withCredentials: false, // Ensure cookies are sent with cross-origin requests
 });
 
 export default axiosInstance;
