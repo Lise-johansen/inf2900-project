43040dc6926f42083b7e589b42f94d6df5877e55
Heine Lysfjord <hly023@uit.no>
2024-03-18 16:01:05 +0100

    Added subject field, also fixed some styling. more centered now.

--- vue-app/src/components/ContactUs.vue
+++ vue-app/src/components/ContactUs.vue
@@ -8,13 +8,15 @@
         <p><strong>Email:</strong> {{ user.email }}</p>
     </div>
     <div>
+        <label for="subject">Subject:</label>
+        <textarea id="subject" v-model="subject" rows="1" cols="50" class="message-box"></textarea>
         <label for="message">Message:</label>
         <textarea id="message" v-model="message" rows="4" cols="50" class="message-box"></textarea>
     </div>
         <button @click="sendMessage">Send Message</button>
     </div>
     <div v-else>
-        <p>Please log in to contact us.</p>
+        <p>Please log in first to contact us.</p>
         <router-link to="/login" class="login-link">Log In</router-link>
     </div>
 </template>
@@ -58,13 +60,15 @@
 </script>
   
 <style scoped>
-    .contact-form {
+.contact-form {
         font-family: 'louis_george_cafe', sans-serif;
         max-width: 500px;
         margin: 0 auto;
         padding: 20px;
         border: 1px solid #ccc;
         border-radius: 5px;
+        display: flex;
+        flex-direction: column;
     }
 
     .contact-form h2 {
@@ -83,6 +87,16 @@
         font-size: x-large;
     }
 
+    #subject {
+        width: 100%;
+        height: auto; /* Allow height to adjust based on content */
+        max-height: 30px; /* Limit the height to one line */
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        resize: none; /* Disable resizing */
+        overflow: hidden; /* Hide overflow content */
+    }
+    
     .contact-form textarea {
         width: 100%;
         padding: 10px;
@@ -107,17 +121,19 @@
         background-color: #0056b3;
     }
 
+    div{
+    display:flex;
+    justify-content:center;
+    align-items: center;
+    flex-direction: column;
+    }
+
     .message-box {
         max-width: 540px;
         height: 150px; /* Fixed height */
         border: 1px solid #ccc;
         border-radius: 5px;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        display: flex;
-        gap: 1em;
-        resize: none;
+        resize: none; /* Disable resizing */   
     }
 
     .login-link {
