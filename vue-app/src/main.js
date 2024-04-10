fa62187f5a80def7a4760d62a8f8eb00357ba38d
Paalsb <72149187+Paalsb@users.noreply.github.com>
2024-04-10 14:32:12 +0100

    Update main.js

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -2,11 +2,26 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas} from '@fortawesome/free-solid-svg-icons';
 import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/lara-light-amber/theme.css'
 
 axios.defaults.baseURL = 'https://django.dybedahlserver.net/api/';
 axios.defaults.withCredentials = true;
-import 'primevue/resources/themes/lara-light-amber/theme.css'
+
 const app = createApp(App);
+
+library.add(fas);
+
+
+// Use router
+app.use(router);
 app.use(PrimeVue);
-app.use(router).mount('#app');
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+// Mount the app
+app.mount('#app');
+
