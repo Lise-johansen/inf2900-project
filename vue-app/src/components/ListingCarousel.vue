22982608a7739578347c4bc4317f94c215a9409f
Dybedahl <adriandybdal@outlook.com>
2024-04-15 09:16:34 +0200

    Think this change will fix the test

--- vue-app/src/components/ListingCarousel.vue
+++ vue-app/src/components/ListingCarousel.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import 'primeicons/primeicons.css'
+import 'primeicons/primeicons.css';
 import axios from 'axios';
 import Carousel from 'primevue/carousel';
 import Button from 'primevue/button';
