e32d32b8bc0759276ec1b3511fd6330c9cab1c5b
Dybedahl <adriandybdal@outlook.com>
2024-04-29 02:23:31 +0200

    Add image field to Message model

--- app_project/airfinn/models.py
+++ app_project/airfinn/models.py
@@ -84,4 +84,5 @@ class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     message = models.TextField(default='')
+    image = models.URLField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
