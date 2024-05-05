a4153710c7566e729afe9c180152d6d8676fcf42
Dybedahl <adriandybdal@outlook.com>
2024-05-05 23:06:12 +0200

    Add get_user API endpoint
    This commit adds a new API endpoint called `get_user` to the `urls.py` file in the `app_project` app. The endpoint is used to retrieve the user ID.

--- app_project/app_project/urls.py
+++ app_project/app_project/urls.py
@@ -52,4 +52,5 @@ urlpatterns = [
     path('api/get-favourites/', views.get_favourites, name='get_favourites'),
     path('api/remove-from-favourites/<int:item_id>/', views.remove_favourites, name='remove_favourites'),
     path('api/verify-user/', views.verify_user, name='verify_user'),
+    path('api/get-user/', views.get_user, name='get_user'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
